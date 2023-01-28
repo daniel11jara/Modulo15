411a85e5b4227bcfd3727aefbe89a527f0314ac8
daniel11jara@gmail.com <danie@10.0.0.103>
2023-01-28 16:13:15 -0300

    aula 15.6

---
+++ modulo15/src/modulo15/DatasEmJava4.java
@@ -0,0 +1,15 @@
+package modulo15;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.text.ParseException;
+
+public class DatasEmJava4 {
+
+	public static void main(String[] args) throws ParseException {
+		// TODO Auto-generated method stub
+		
+		Date dataPassada = new SimpleDateFormat("dd/MM/yyyy").parse("01/02/2021");
+	}
+
+}
