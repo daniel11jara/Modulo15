233ff18fe16fb665aaa82c0480ece598b5bce9fb
daniel11jara@gmail.com <danie@10.0.0.103>
2023-01-28 16:30:39 -0300

    aula 15.6

--- modulo15/src/modulo15/DatasEmJava4.java
+++ modulo15/src/modulo15/DatasEmJava4.java
@@ -1,15 +1,23 @@
 package modulo15;
 
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.Date;
 import java.text.ParseException;
 
 public class DatasEmJava4 {
 
 	public static void main(String[] args) throws ParseException {
-		// TODO Auto-generated method stub
 		
-		Date dataPassada = new SimpleDateFormat("dd/MM/yyyy").parse("01/02/2021");
+		//aula 15.6
+		
+		//chronounit é um pacote estático não precisa instanciar
+		//dias entre 01/02/2021 e hoje
+		//retorna um long
+		long dias = ChronoUnit.DAYS.between(LocalDate.parse("2021-02-01"), LocalDate.now());
+		
+		System.out.println("Possui " + dias + " entre a faixa de datas");
 	}
 
 }
